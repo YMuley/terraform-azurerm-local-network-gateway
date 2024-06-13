6c2da64566ef7ee23ba4966acd9f687e1c1f615c
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2024-06-13 20:06:27 +0530

    code update

--- main.tf
+++ main.tf
@@ -3,11 +3,13 @@ resource "azurerm_local_network_gateway" "local_gateway" {
     name                    = each.value.name
     resource_group_name     = var.resource_group_output[each.value.resource_group_name].name
     location                = var.resource_group_output[each.value.resource_group_name].location
-    gateway_address         = each.value.gateway_address
-    address_space           = each.value.address_space
+    gateway_address         = length(each.value.gateway_fqdn) > 0 && each.value.gateway_fqdn != null ? null : each.value.gateway_address
+    gateway_fqdn            = length(each.value.gateway_address) > 0 &&  each.value.gateway_address != null ? null : each.value.gateway_fqdn
+    address_space           = each.value.enable_bgp != true ? each.value.gateway_address: null
+    tags                    = each.value.tags
 
     dynamic "bgp_settings" {
-      for_each = each.value.bgp_settings == [] ? [] : each.value.bgp_settings
+      for_each = each.value.enable_bgp == true ? each.value.bgp_settings : []
       content {
         asn                     = bgp_settings.value.asn
         bgp_peering_address     = bgp_settings.value.bgp_peering_address
