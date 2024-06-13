699b6365c688cc92a7fd8df893deb92a2749c737
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2024-06-13 17:13:24 +0530

    first commit

--- locals.tf
+++ locals.tf
@@ -1,3 +1,3 @@
 locals {
-  gateway = { for gateway in var.var.local_network_gateway_list : gateway.name => gateway}
+  gateway = { for gateway in var.local_network_gateway_list : gateway.name => gateway}
 }
